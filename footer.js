917fa82b3717853ed45d53bcf757a63f6c2a6310
alyzaraviandi <alyzaraviandi@gmail.com>
2024-05-25 14:55:00 +0700

    fix alyyyy

--- footer.js
+++ footer.js
@@ -1,24 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Create a new footer element
-    var footer = document.createElement("footer");
-    
-    // Create a div element for the copyright text
-    var copyrightDiv = document.createElement("div");
-    copyrightDiv.classList.add("copyright");
-
-    // Create a paragraph element for the copyright text
-    var copyrightText = document.createElement("p");
-    copyrightText.innerHTML = "&copy; 2024 Team 20 Semarang. All rights reserved.";
-
-    // Append the paragraph element to the div
-    copyrightDiv.appendChild(copyrightText);
-
-    // Append the div to the footer element
-    footer.appendChild(copyrightDiv);
-
-    // Get the footer element by its id
-    var footerContainer = document.getElementById("footer");
-
-    // Append the footer element to the footer container
-    footerContainer.appendChild(footer);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'footer.html', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById('footer').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
 });
